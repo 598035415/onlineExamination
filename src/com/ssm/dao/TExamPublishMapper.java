12d85a986d5b2558bf4234ba8f9fd54c97947676
374288894@qq.com <chenlei@znsd.com>
2019-09-06 16:33:25 +0800

    a

--- src/com/ssm/dao/TExamPublishMapper.java
+++ src/com/ssm/dao/TExamPublishMapper.java
@@ -1,17 +1,5 @@
 package com.ssm.dao;
 
-import com.ssm.pojo.TExamPublish;
 
 public interface TExamPublishMapper {
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(TExamPublish record);
-
-    int insertSelective(TExamPublish record);
-
-    TExamPublish selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(TExamPublish record);
-
-    int updateByPrimaryKey(TExamPublish record);
 }
--- src/com/ssm/dao/TStudentExamAnswerMapper.java
+++ src/com/ssm/dao/TStudentExamAnswerMapper.java
@@ -1,17 +1,5 @@
 package com.ssm.dao;
 
-import com.ssm.pojo.TStudentExamAnswer;
 
 public interface TStudentExamAnswerMapper {
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(TStudentExamAnswer record);
-
-    int insertSelective(TStudentExamAnswer record);
-
-    TStudentExamAnswer selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(TStudentExamAnswer record);
-
-    int updateByPrimaryKey(TStudentExamAnswer record);
 }
