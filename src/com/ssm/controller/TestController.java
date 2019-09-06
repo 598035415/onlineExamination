bc30b49160362915ca0fe9328a662e536f5044e2
tm <598035415@qq.com>
2019-09-06 19:58:47 +0800

    # WARNING: head commit changed in the meantime
    log

--- src/com/ssm/controller/TestController.java
+++ src/com/ssm/controller/TestController.java
@@ -1,20 +1,39 @@
 package com.ssm.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ssm.dao.TestDao;
 
+import main.resources.Test;
+
 @Controller
 public class TestController {
 	@Autowired
 	TestDao  td;
 	
+//	public static Logger lo =Logger.getLogger(Test.class);
+
+	
 	@RequestMapping("a")
 	public void test() {
 		System.out.println("===================");
-		System.out.println(   td.selectAll() );
+	
+		
+		System.out.println(  td.selectAll()  );
+		
+		for (int i = 0; i < 100; i++) {
+		//	lo.info("==========================================================");
+		}
+		
+		for (int i = 0; i < 100; i++) {
+	//		lo.debug("-----------------------------------------------------");
+		}
+		
+		
+		
 	}
 	
 	
