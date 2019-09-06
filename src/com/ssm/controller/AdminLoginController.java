470ae17df3f8643b5c93304acc7d7d7b4deec079
Administrator <Administrator@PC-201907082022>
2019-09-06 19:41:54 +0800

    00

--- src/com/ssm/controller/AdminLoginController.java
+++ src/com/ssm/controller/AdminLoginController.java
@@ -1,5 +1,8 @@
 package com.ssm.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +18,16 @@ public class AdminLoginController {
 	private TUserService service;
 	
 	@RequestMapping("/adminLogin")
-	public String adminLogin(String username,String password) {
-		ResponseEntity<TUser> backgroundLogin = service.backgroundLogin(username, password);
-		
-		
-		
+	public String adminLogin(HttpServletRequest request,String username,String password) {
+		ResponseEntity<TUser> entity = service.backgroundLogin(username, password);
+		System.out.println(entity.getData());
+		System.out.println(entity.getMsg());
+		if("200".equals(entity.getCode())) {
+			HttpSession session = request.getSession();
+			session.setAttribute("user",entity.getData());
+		}else {
+			
+		}
 		return null;
 	}
 }
