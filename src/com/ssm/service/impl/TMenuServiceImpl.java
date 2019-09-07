67f60141aec9a7a3080240b7d0e10bcd69ba327f
Administrator <Administrator@PC-201907082022>
2019-09-07 08:58:31 +0800

    00

--- src/com/ssm/service/impl/TMenuServiceImpl.java
+++ src/com/ssm/service/impl/TMenuServiceImpl.java
@@ -19,9 +19,35 @@ public class TMenuServiceImpl implements TMenuService {
 	List<MenuJson> menuJsonList;
 	
 	@Override
-	public List<MenuJson> selectById(Integer userId) {
-		List<TMenu> menuList = dao.selectById(userId);
-		return null;
+	public List<TMenu> selectById(Integer userId) {
+		return dao.selectById(userId);
+	}
+
+	@Override
+	public List<TMenu> selectAll() {
+		return dao.selectAll();
+	} 
+	
+	public List<MenuJson> selectMenu(Integer userId){
+		List<TMenu> selectById = selectById(userId);
+		List<TMenu> k = selectAll();
+		for (TMenu tMenu : selectById) {
+			MenuJson json = new MenuJson();
+			json.setId(tMenu.getId());
+			json.setName(tMenu.getMenuName());
+			json.setUrl(tMenu.getUrl());
+			for (TMenu tMenu2 : k) {
+				MenuJson jsont = new MenuJson();
+				if(tMenu2.getParentId()==tMenu.getId()) {
+					jsont.setId(tMenu2.getId());
+					jsont.setName(tMenu2.getMenuName());
+					jsont.setUrl(tMenu2.getUrl());
+					json.getChildren().add(jsont);
+				}
+				menuJsonList.add(json);
+			}
+		}
+		return menuJsonList;	
 	}
 
 }
