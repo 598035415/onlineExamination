368d8476b4b00298aaee01e5fcb5ef08c4dafbd6
tm <598035415@qq.com>
2019-09-07 11:09:08 +0800

    dict  模块

--- src/com/ssm/util/Page.java
+++ src/com/ssm/util/Page.java
@@ -7,7 +7,13 @@ public class Page {
 	private Integer page;
 	private Integer limit;
 	
+	// 数据字典
+	private String type;   //类型
+	private String label;  //标签名
 	
-	
+	//日志
+	private String loginName;  // 姓名
+	private String startTime;  //开始时间
+	private String endTime;	   //结束时间
 	
 }
